6b1e9e354d38758510b640706e778e2a2dabbb32
K.Nagata <k_nagata@privatembp.local>
2024-01-07 23:26:47 +0900

    commit

--- resources/ts/index.tsx
+++ resources/ts/index.tsx
@@ -6,12 +6,15 @@ const container = document.getElementById('app');
 const root = createRoot(container!);
 
 root.render(
-    <div className="text-red">
-        このサイトで使用している開発言語
-        <br></br>
-        ・PHP 8.2(Laravel 10)
-        <br></br>
-        ・React 18.2+TypeScript 5.3
-        <br></br>
+    <div className="text-medium-bold">
+        GitHub:<a href="https://github.com/nagatakodai/portfolio">https://github.com/nagatakodai/portfolio</a><br></br>
+        このサイトで使用している開発環境<br></br>
+        バックエンド<br></br>
+        ・PHP 8.2(Laravel 10)<br></br>
+        ・MySQL 8.0<br></br>
+        フロントエンド<br></br>
+        ・React 18.2+TypeScript 5.3<br></br>
+        インフラ<br></br>
+        ・AWS EC2<br></br>
     </div>
 );
