7c1a933b5f8dbea1395342ebc96184bfeedaf251
K.Nagata <k_nagata@privatembp.local>
2024-01-15 18:23:12 +0900

    開発環境の更新

--- resources/ts/index.tsx
+++ resources/ts/index.tsx
@@ -8,13 +8,18 @@ const root = createRoot(container!);
 root.render(
     <div className="text-medium-bold">
         GitHub:<a href="https://github.com/nagatakodai/portfolio">https://github.com/nagatakodai/portfolio</a><br></br>
-        このサイトで使用している開発環境<br></br>
-        バックエンド<br></br>
-        ・PHP 8.2(Laravel 10)<br></br>
-        ・MySQL 8.0<br></br>
-        フロントエンド<br></br>
-        ・React 18.2+TypeScript 5.3<br></br>
-        インフラ<br></br>
+        <br></br>このプロジェクトで使用している開発環境<br></br>
+        <br></br>バックエンド<br></br>
+        ・PHP 8.x(Laravel 10.x)<br></br>
+        ・MySQL 8.x<br></br>
+        <br></br>フロントエンド<br></br>
+        ・React 18.x+TypeScript 5.x<br></br>
+        <br></br>デプロイ環境<br></br>
+        <br></br>CI/CD<br></br>
+        ・GitHub Actions<br></br>
+        <br></br>サーバー<br></br>
         ・AWS EC2<br></br>
+        <br></br>DB<br></br>
+        ・AWS RDS
     </div>
 );
