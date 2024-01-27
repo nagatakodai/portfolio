3e729fcbb6774bbbc8338a486e71aaa59690e9bb
K.Nagata <k_nagata@privatembp.local>
2024-01-28 07:48:57 +0900

    ページ追加

--- resources/ts/pages/Index.tsx
+++ resources/ts/pages/Index.tsx
@@ -5,21 +5,25 @@ import Button from '@mui/material/Button';
 const Index = () => {
     return (
         <div className="text-medium-bold">
-            <Button onClick={() => alert('Hello, World!')}>Click me!</Button><br /><br />
-            GitHub:<a href="https://github.com/nagatakodai/portfolio">https://github.com/nagatakodai/portfolio</a><br /><br />
-            このプロジェクトで使用している開発環境<br /><br />
-            バックエンド<br />
-            ・PHP 8.x(Laravel 10.x)<br />
-            ・MySQL 8.x<br /><br />
-            フロントエンド<br />
-            ・React 18.x+TypeScript 5.x<br /><br />
-            デプロイ環境<br /><br />
-            CI/CD<br />
-            ・GitHub Actions<br /><br />
-            サーバー<br />
-            ・AWS EC2<br /><br />
-            DB<br />
+            <h1>Index Page</h1>
+            <Button onClick={() => alert('マテリアルUIのボタンだよ')}>Click me!</Button><br/><br/>
+            GitHub:<a
+            href="https://github.com/nagatakodai/portfolio">https://github.com/nagatakodai/portfolio</a><br/><br/>
+            このプロジェクトで使用している開発環境<br/><br/>
+            バックエンド<br/>
+            ・PHP 8.x(Laravel 10.x)<br/>
+            ・MySQL 8.x<br/><br/>
+            フロントエンド<br/>
+            ・React 18.x+TypeScript 5.x<br/><br/>
+            デプロイ環境<br/><br/>
+            CI/CD<br/>
+            ・GitHub Actions<br/><br/>
+            サーバー<br/>
+            ・AWS EC2<br/><br/>
+            DB<br/>
             ・AWS RDS
+
+
         </div>
     );
 }
